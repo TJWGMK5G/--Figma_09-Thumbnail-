f70527cd497b20b3d5ca31e7fb207ca55bd6d94a
TJWGMK5G <87062165+TJWGMK5G@users.noreply.github.com>
2022-03-30 15:47:44 +0300

    123

--- html/script.js
+++ html/script.js
@@ -4,3 +4,29 @@ $(document).ready(function () {
     $("body").toggleClass("lock");
   });
 });
+
+$(".center").slick({
+  centerMode: true,
+  centerPadding: "60px",
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
